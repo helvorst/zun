780524b59f5f88a4987297b7a91b2b6b78a1d92f
elena <elena_barantseva@epam.com>
2017-07-25 19:14:15 +0200

    Playlist + player

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
+import { HttpModule } from '@angular/http';
+import { PlaylistComponent } from './youtube/playlist/playlist.component';
+import { YoutubeService } from './service/youtube.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import 'rxjs/Rx';
 
 import { AppComponent } from './app.component';
+import { VideoComponent } from './youtube/video/video.component';
+import { PlayerComponent } from './youtube/player/player.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlaylistComponent,
+    VideoComponent,
+    PlayerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [YoutubeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
