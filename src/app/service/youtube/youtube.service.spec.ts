8e1f71c1c5474b371d238c48893367b30faa3914
elena <elena_barantseva@epam.com>
2017-08-01 18:50:25 +0200

    YoutubeService test of GET method

--- src/app/service/youtube/youtube.service.spec.ts
+++ src/app/service/youtube/youtube.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed, inject } from '@angular/core/testing';
 //import { HttpModule } from '@angular/http';
-import { Http, HttpModule, XHRBackend } from '@angular/http';
+import { Connection, Http, HttpModule, ResponseOptions, Response, XHRBackend } from '@angular/http';
 import { YoutubeService } from './youtube.service';
 import { MockBackend } from '@angular/http/testing';
 
@@ -18,4 +18,21 @@ describe('YoutubeService', () => {
   it('should be created', inject([YoutubeService], (service: YoutubeService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should get videos', inject([YoutubeService, XHRBackend], (service: YoutubeService, mock: MockBackend) => {
+    var fakeItems = {
+      items: [{ id: 0, name: 'gaga' }]
+    };
+    mock.connections.subscribe(connection => {
+      connection.mockRespond(new Response(new ResponseOptions({
+        body: JSON.stringify(fakeItems)
+      })));
+    });
+    service.getPlaylistItems()
+      .subscribe(result => {
+        for (var i in result) {
+          expect(result[i]).toEqual(fakeItems.items[i]);
+        }
+      });
+  }))
 });
