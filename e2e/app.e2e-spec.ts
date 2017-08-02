98e6b37ae4875c571f507bff36bd899c3dabe702
elena <elena_barantseva@epam.com>
2017-08-02 18:41:27 +0200

    protractor

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,27 +1,41 @@
 import { ZunPage } from './app.po';
-import { element, by } from "protractor/built";
+import { browser, by, element } from 'protractor/built';
 
 describe('zun App', () => {
   let page: ZunPage;
-   page = new ZunPage();
-
+  page = new ZunPage();
+  page.navigateTo();
+  
   // beforeEach(() => {
   //   page = new ZunPage();
+  //   page.navigateTo();
   // });
 
-debugger
-  it('should display title', (done) => {
-    page.navigateTo();
-    const title = element(by.css('.header-title'));
-    expect(title.isPresent()).toBeTruthy();
-    done();
-    //expect(page.getParagraphText()).toEqual('Welcome to app!');
-  });
-   it('should display a list of videos', (done) => {
-    page.navigateTo();
-    debugger
-    expect(element(by.css('.video')).length > 250).toBe(true);
-    done();
-  });
+  // it('should display title', () => {
+  //   //browser.pause()
+  //   const title = element(by.css('.header-title'));
+  //   expect(title.isPresent()).toBeTruthy();
+  // });
+  //  it('should display a list of videos', () => {
+  //   expect(element.all(by.css('.video')).count()).toBeTruthy();
+  // });
+  // it('should select video', () => {
+  //   var player = element(by.id('ytplayer'));
+  //   var video = element.all(by.css('.video')).get(0);
+  //   video.click();
+  //   var player2 = element(by.id('ytplayer'));
+  //   expect(player).not.toEqual(player2);
+  // });c
+  it('should navigate videos with buttons', (done) => {
+    // const btnNext = element(by.id('btnNext'));
+    // const btnPrevious = element(by.id('btnPrevious'));
+    const song = element(by.css('.player-title')).getText();
+    //btnNext.click(); 
+    var video = element.all(by.css('.video')).get(0);
+    video.click();
+    const song2 = element(by.css('.player-title')).getText();      
+    expect(song).not.toEqual(song2);
   
+  })
+
 });
